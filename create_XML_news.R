fe6fbca2e147cd1c8b6a6192f8771e9634b91ac2
awp-finanznachrichten <sw@awp.ch>
2025-08-26 12:03:11 +0200

    commit from Rstudio

--- create_XML_news.R
+++ create_XML_news.R
@@ -17,13 +17,13 @@ write_file(item,paste0(OUTPUT_PATH_NEWS,ID,".xml"))
 
 #Entry in Database
 selected_category <- paste0(category,"/",type,"/",name)
-mydb <- retry(connectDB(db_name="picture_delivery_robot"),sleep=5)
-sql_qry <- "INSERT IGNORE INTO picture_delivering_overview(picture_name,delivering_time,news_id,customer,title,text,options_categories,selected_category,selection_process) VALUES"
-sql_qry <- paste0(sql_qry, paste(sprintf("('%s','%s','%s','%s','%s','%s','%s','%s','%s')",
-                                         picture_name,Sys.time(),ID,"Investora",titel,text,
-                                         options_pictures,
-                                         str_replace_all(selected_category, "\\'", "\\\\'"),
-                                         str_replace_all(selection_process, "\\'", "\\\\'")), collapse = ","))
-dbGetQuery(mydb, "SET NAMES 'utf8'")
-rs <- retry(dbSendQuery(mydb, sql_qry),sleep=5)
-dbDisconnectAll()
+# mydb <- retry(connectDB(db_name="picture_delivery_robot"),sleep=5)
+# sql_qry <- "INSERT IGNORE INTO picture_delivering_overview(picture_name,delivering_time,news_id,customer,title,text,options_categories,selected_category,selection_process) VALUES"
+# sql_qry <- paste0(sql_qry, paste(sprintf("('%s','%s','%s','%s','%s','%s','%s','%s','%s')",
+#                                          picture_name,Sys.time(),ID,"Investora",titel,text,
+#                                          options_pictures,
+#                                          str_replace_all(selected_category, "\\'", "\\\\'"),
+#                                          str_replace_all(selection_process, "\\'", "\\\\'")), collapse = ","))
+# dbGetQuery(mydb, "SET NAMES 'utf8'")
+# rs <- retry(dbSendQuery(mydb, sql_qry),sleep=5)
+# dbDisconnectAll()
